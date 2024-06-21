bf29cb9f568496a405313cd219e2dc6b8a6543d5
IkbalKulahcioglu <ikbalkulahcioglu@gmail.com>
2024-06-21 22:54:37 +0300

    Pulled data from store using useSelector and edited navbar css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { GiCupcake } from "react-icons/gi";
+import { useSelector } from "react-redux";
+
+
 function Navbar() {
+
+  const {quantity} = useSelector((store) => store.cart);
   return (
     <nav>
       <div className="navbar">
         <h3>Muffin Store</h3>
         <div className="navDiv">
-          <p>0</p>
-          <GiCupcake className="" />
+          <div className="itemDiv">
+            <p>{quantity}</p>
+          </div>
+
+          <GiCupcake className="itemsIcon" />
         </div>
       </div>
     </nav>
